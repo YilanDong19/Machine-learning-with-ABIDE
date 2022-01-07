9310cb980da51d8ae80dd33c048e9d8635f185a8
Yilan Dong <56785684+YilanDong19@users.noreply.github.com>
2022-01-07 15:52:33 +0000

    Update prepare_ABIDE.py

--- GCN_ABIDE/prepare_ABIDE.py
+++ GCN_ABIDE/prepare_ABIDE.py
@@ -18,15 +18,6 @@ def flatten_one(img):
     return one_line
 
 def feature_selection(matrix, labels, train_ind, fnum):
-    """
-        matrix       : feature matrix (num_subjects x num_features)
-        labels       : ground truth labels (num_subjects x 1)
-        train_ind    : indices of the training samples 
-        fnum         : size of the feature vector after feature selection
-
-    return:
-        x_data      : feature matrix of lower dimension (num_subjects x fnum)
-    """
 
     estimator = RidgeClassifier()
     selector = RFE(estimator, fnum, step=100, verbose=1)
