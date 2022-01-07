a032e4a954da6ec1c0320e79cc1f03683e90c4d5
Yilan Dong <56785684+YilanDong19@users.noreply.github.com>
2022-01-07 15:54:23 +0000

    Update feature_selection.py

--- SVM_ABIDE/feature_selection.py
+++ SVM_ABIDE/feature_selection.py
@@ -7,22 +7,12 @@ import os
 import scipy.io as scio
 
 def feature_selection(matrix, labels, train_ind, fnum):
-    """
-        matrix       : feature matrix (num_subjects x num_features)
-        labels       : ground truth labels (num_subjects x 1)
-        train_ind    : indices of the training samples 
-        fnum         : size of the feature vector after feature selection
-
-    return:
-        x_data      : feature matrix of lower dimension (num_subjects x fnum)
-    """
-
+    
     estimator = RidgeClassifier()
     selector = RFE(estimator, fnum, step=100, verbose=1)
 
     featureX = matrix[train_ind, :]
     featureY = labels[train_ind]
     selector = selector.fit(featureX, featureY.ravel())
-    # x_data = selector.transform(matrix)
 
     return selector
